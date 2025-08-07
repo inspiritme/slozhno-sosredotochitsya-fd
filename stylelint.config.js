a3cd4b118abc82a5e71f8f47040241e108b282ff
Adlet Koshanov <adletkoshanov@gmail.com>
2025-08-07 11:44:37 +0500

    feature footer

--- stylelint.config.js
+++ stylelint.config.js
@@ -26,5 +26,6 @@ module.exports = {
     'at-rule-empty-line-before': 'always',
     'selector-class-pattern': null,
     'font-family-name-quotes': 'always-where-recommended',
+    'rule-empty-line-before' : "never",
   },
 }
